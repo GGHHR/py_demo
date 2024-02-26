24bfd311d0651081fcb164c5eea623a3e3f7e5c3
2 <1241639333@qq.com>
2024-02-26 15:24:48 +0800

    1

--- v2rayN订阅更新.py
+++ v2rayN订阅更新.py
@@ -5,6 +5,7 @@ import psutil
 from pathlib import Path
 import json
 import re
+import os
 
 async def get_running_v2rayn_root_path():
     for proc in psutil.process_iter(attrs=['pid', 'name']):
@@ -28,31 +29,34 @@ async def up_sub_item(url, remarks, id, convert_target, command):
         print('v2rayn is not running')
 
 async def sub_get(browser, url, sel, remarks, id):
-    convert_target = ""
-    if url.endswith(("yaml", "yml")):
-        convert_target = "mixed"
-    print(id, url)
-    await up_sub_item(url, remarks, id, convert_target, await get_running_v2rayn_root_path())
 
     if sel is not None:
         page = await browser.newPage()
-        await page.goto(url, {'timeout': 99999})
+        await page.goto(url,{'timeout': 99999})
         content = ''
         if sel[0]:
-            await page.waitForSelector(sel[0], {'timeout': 99999})
+            await page.waitForSelector(sel[0],{'timeout': 99999})
             content = await page.evaluate(f'(element) => document.querySelector("{sel[0]}").href')
-            await page.goto(content, {'timeout': 99999})
+            await page.goto(content,{'timeout': 99999})
 
-        await page.waitForSelector(sel[1], {'timeout': 99999})
+        await page.waitForSelector(sel[1],{'timeout': 99999})
         content = await page.evaluate(f'(element) => document.querySelector("{sel[1]}").textContent')
 
         url_pattern = r'https?://[^\s/$.?#].[^\s]*'
         match = re.search(url_pattern, content)[0]
+        convert_target=""
         if match.endswith(("yaml", "yml")):
             convert_target = "mixed"
         print(remarks, match)
         await up_sub_item(match, remarks, id, convert_target, await get_running_v2rayn_root_path())
         await page.close()
+    else:
+        convert_target = ""
+        if url.endswith(("yaml", "yml")):
+            convert_target = "mixed"
+        print(id, url)
+        await up_sub_item(url, remarks, id, convert_target, await get_running_v2rayn_root_path())
+
 
 
 async def main():
@@ -64,7 +68,10 @@ async def main():
         url = 'https://raw.githubusercontent.com/GGHHR/node_demo/master/v2rayn订阅/init.json'
         await page.goto(url, {'timeout': 99999})
         await page.waitForSelector('pre', {'timeout': 99999})
-        content = await page.evaluate('(element) => element.textContent', 'pre')
+        content = await page.evaluate('''() => {
+            const element = document.querySelector('pre');
+            return element ? element.textContent : null;
+        }''')
         select = json.loads(content)
         await page.close()
         print('Request successful')
@@ -73,10 +80,27 @@ async def main():
             select = json.load(f)
         print('Request failed, using local json file')
 
+    await asyncio.gather(*[sub_get(browser, v['url'], v.get('sel'), i + 1, i + 1) for i, v in enumerate(select['select'])])
 
-    print(select)
-
-    await asyncio.gather( *[sub_get(browser, v['url'], v.get('sel'), i + 1, i + 1) for i, v in enumerate(select['select'])])
     await browser.close()
+    try:
+        command = await get_running_v2rayn_root_path()
+        print('命令:', command)
+        if command:
+            output_value = os.path.join(command, 'guiConfigs/guiNDB.db')
+            db = sqlite3.connect(output_value)
+            cursor = db.cursor()
+            delete_sql = f"DELETE FROM SubItem WHERE sort > {len(select['select'])}"
+            print('删除 SQL:', delete_sql)
+            cursor.execute(delete_sql)
+            db.commit()
+            db.close()
+            print(f'成功删除排序大于 {len(select['select'])} 的记录。')
+        else:
+            print('v2rayn 未在运行。')
+    except Exception as error:
+        print('清理操作失败:', error)
+
+
 
 asyncio.run(main())
