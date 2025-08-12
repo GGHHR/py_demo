dc0aa2c8c740a6bfee64d293d38816c3bdaef00b
12416 <1241639333@qq.com>
2025-08-13 00:25:02 +0800

    1

--- v2rayN/v2.py
+++ v2rayN/v2.py
@@ -154,7 +154,7 @@ async def main():
         for i, v in enumerate(select['select']):
             v['id'] = i + 1
 
-        sem = asyncio.Semaphore(5)
+        sem = asyncio.Semaphore(10)
 
         async def task(v, i):
             async with sem:
